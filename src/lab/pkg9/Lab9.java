f0f81323319255f68e6eec6892e5c68b01ef1502
Adelessam12 <Adelessam2004@gmail.com>
2024-11-30 20:47:53 +0200

    mahmoud

--- src/lab/pkg9/Lab9.java
+++ src/lab/pkg9/Lab9.java
@@ -14,7 +14,7 @@ public class Lab9 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
- 
+        System.out.println("hello ");
         
     }
     
