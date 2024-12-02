975c0bb952c17aceb0335f1eb4e7af42180aa717
Adelessam12 <Adelessam2004@gmail.com>
2024-12-02 22:48:42 +0200

    Update ContentDatabase.java

--- src/lab/pkg9/ContentDatabase.java
+++ src/lab/pkg9/ContentDatabase.java
@@ -16,7 +16,6 @@ public class ContentDatabase {
         this.storiesFilePath = storiesFilePath;
     }
 
-    // Save Posts to JSON file
     public void savePosts(ArrayList<Post> posts) throws IOException {
         try (Writer writer = new FileWriter(postsFilePath)) {
             Gson gson = new Gson();
@@ -24,18 +23,16 @@ public class ContentDatabase {
         }
     }
 
-    // Load Posts from JSON file
     public ArrayList<Post> loadPosts() throws IOException {
         try (Reader reader = new FileReader(postsFilePath)) {
             Gson gson = new Gson();
             Type postListType = new TypeToken<ArrayList<Post>>() {}.getType();
             return gson.fromJson(reader, postListType);
         } catch (FileNotFoundException e) {
-            return new ArrayList<>(); // Return empty list if file doesn't exist
+            return new ArrayList<>();
         }
     }
 
-    // Save Stories to JSON file
     public void saveStories(ArrayList<Story> stories) throws IOException {
         try (Writer writer = new FileWriter(storiesFilePath)) {
             Gson gson = new Gson();
@@ -43,14 +40,13 @@ public class ContentDatabase {
         }
     }
 
-    // Load Stories from JSON file
     public ArrayList<Story> loadStories() throws IOException {
         try (Reader reader = new FileReader(storiesFilePath)) {
             Gson gson = new Gson();
             Type storyListType = new TypeToken<ArrayList<Story>>() {}.getType();
             return gson.fromJson(reader, storyListType);
         } catch (FileNotFoundException e) {
-            return new ArrayList<>(); // Return empty list if file doesn't exist
+            return new ArrayList<>();
         }
     }
 }
