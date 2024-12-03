658aec873792ea31f6f9f859bc6e967d86b1ff83
AhmedAlaa8100 <ahmedalaa8100@gmail.com>
2024-12-03 22:44:19 +0200

    updated
    updated friendManegment + user classes.
    
    Co-Authored-By: MahmoudWaleed1 <142265894+MahmoudWaleed1@users.noreply.github.com>

--- src/lab/pkg9/Friend_Management.java
+++ src/lab/pkg9/Friend_Management.java
@@ -5,6 +5,7 @@
 package lab.pkg9;
 
 import java.util.ArrayList;
+import java.util.Map;
 
 /**
  *
@@ -13,52 +14,64 @@ import java.util.ArrayList;
 public class Friend_Management {
     Db db;
 
+    Db db;
+    
     public Friend_Management(Db db) {
         this.db = db;
     }
-    
-    public void sendFriendRequest(User user, User friend){
-        user.setSentFriendRequestStatus("Pending");
-        friend.setReceivedFriendRequestStatus("Pending");
-        
+
+    public void setSentRequestStatus(User friend, User user, String status) {
+        Map m = friend.getSentFriendRequestStatus();
+        m.put(user.getUserId(), status);
     }
-    
-     public void acceptFriendRequest(User user, User friend){
-        friend.setReceivedFriendRequestStatus("accepted");
-        user.setSentFriendRequestStatus("accepted");
-        user.friendList.add(friend);
-        friend.friendList.add(user);
+
+    public void setRecievedRequestStatus(User user, User friend, String status) {
+        Map m = friend.getReceivedFriendRequestStatus();
+        m.put(user.getUsername(), status);
     }
-     
-    public void declineFriendRequest(User user, User friend){
-        user.setSentFriendRequestStatus("declined");
-        friend.setSentFriendRequestStatus("deleted");
+
+    public void sendFriendRequest(User user, User friend) {
+        setSentRequestStatus(user, friend, "Pending");
+        setSentRequestStatus(friend, user, "Pending");
     }
-    
-    public void removeFriend(User user, User friend){
-        user.friendList.remove(friend);
-        friend.friendList.remove(user);
+
+    public void acceptFriendRequest(User user, User friend) {
+        setRecievedRequestStatus(friend, user, "Accepted");
+        setSentRequestStatus(user, friend, "Accepted");
+        user.addFriend(friend);
+        friend.addFriend(user);
     }
-    
-    public void blockFriend(User user, User friend){
-        user.friendList.remove(friend);
-        friend.friendList.remove(user);
-        user.blockedList.add(friend);
+
+    public void declineFriendRequest(User user, User friend) {
+        setSentRequestStatus(user, friend, "Declined");
+        setRecievedRequestStatus(friend, user, "Declined");
     }
-    
-    public ArrayList<User> suggestions(User user){
+
+    public void removeFriend(User user, User friend) {
+        user.removeFriend(friend);
+        friend.removeFriend(user);
+    }
+
+    public void blockFriend(User user, User friend) {
+        user.removeFriend(friend);
+        friend.removeFriend(user);
+        user.addToBlocked(friend);
+    }
+
+    public ArrayList<User> suggestions(User user) {
         ArrayList<User> suggestions = new ArrayList<>();
-        for(User suggested: db.getUsers()){
-            if(!db.getUsers().contains(user)&&!suggested.equals(user)){
-                suggestions.add(user);
-                for(User commonFriend: user.friendList){
-                    if(suggested.friendList.contains(commonFriend))
-                        System.out.println("Has mustual friends");
+        for (User suggested : db.getUsers()) {
+            if (!suggested.equals(user) && !user.getFriendList().contains(suggested) && !user.getBlockedList().contains(suggested)) {
+                suggestions.add(suggested);
+                for (User commonFriend : user.getFriendList()) {
+                    if (suggested.getFriendList().contains(commonFriend)) {
+                        System.out.println("Has mutual friends");
+                    }
                 }
-            
+
             }
         }
         return suggestions;
     }
-    
+
 }
