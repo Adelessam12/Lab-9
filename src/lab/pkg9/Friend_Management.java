b4a4b40e658b638b4a41a148deaf160f6c647915
Mahmoud Aboelsoaoud <mahmoudaboelsoaoud@gmail.com>
2024-12-03 18:54:23 +0200

    part 4 but slight changes are left

--- src/lab/pkg9/Friend_Management.java
+++ src/lab/pkg9/Friend_Management.java
@@ -4,35 +4,55 @@
  */
 package lab.pkg9;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author Mahmoud Waleed
  */
 public class Friend_Management {    
-    public void sendFriendRequest(Friend friend, User user){
-        friend.setFriendRequestStatus("Pending");
+    public void sendFriendRequest(User user, User friend){
+        user.setSentFriendRequestStatus("Pending");
+        friend.setReceivedFriendRequestStatus("Pending");
         
     }
     
-     public void acceptFriendRequest(User user, Friend friend){
-        friend.setFriendRequestStatus("accepted");
-        user.friendList.add(friend.getFriend());
-        friend.getFriend().friendList.add(user);
+     public void acceptFriendRequest(User user, User friend){
+        friend.setReceivedFriendRequestStatus("accepted");
+        user.setSentFriendRequestStatus("accepted");
+        user.friendList.add(friend);
+        friend.friendList.add(user);
     }
      
-    public void declineFriendRequest(User user, Friend friend){
-        friend.setFriendRequestStatus("declined");
+    public void declineFriendRequest(User user, User friend){
+        user.setSentFriendRequestStatus("declined");
+        friend.setSentFriendRequestStatus("deleted");
     }
     
-    public void removeFriend(User user, Friend friend){
-        user.friendList.remove(friend.getFriend());
-        friend.getFriend().friendList.remove(user);
+    public void removeFriend(User user, User friend){
+        user.friendList.remove(friend);
+        friend.friendList.remove(user);
     }
     
-    public void blockFriend(User user, Friend friend){
-        user.friendList.remove(friend.getFriend());
-        friend.getFriend().friendList.remove(user);
+    public void blockFriend(User user, User friend){
+        user.friendList.remove(friend);
+        friend.friendList.remove(user);
+        user.blockedList.add(friend);
     }
     
+    public ArrayList<User> suggestions(User user){
+        ArrayList<User> suggestions = new ArrayList<>();
+        for(User suggested: User.allUsers){
+            if(!User.allUsers.contains(user)&&!suggested.equals(user)){
+                suggestions.add(user);
+                for(User commonFriend: user.friendList){
+                    if(suggested.friendList.contains(commonFriend))
+                        System.out.println("Has mustual friends");
+                }
+            
+            }
+        }
+        return suggestions;
+    }
     
 }
